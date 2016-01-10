c6331d69b2a7c81393b61e738dc881682d4fe4ba
a.harada <redhornet96@gmail.com>
2016-01-10 16:54:07 +0900

    out log

--- main.rb
+++ main.rb
@@ -5,14 +5,19 @@ require 'dotenv'
 Dotenv.load
 
 get '/' do
+  logger.info "index-----------------------------"
   erb :index
 end
 
 get '/call' do
+  logger.info "call-----------------------------"
   TwilioCaller.new.call(params[:tel_number]) if params[:tel_number]
 end
 
 post '/status_callback' do
+  logger.debug "-----------------------"
+  logger.debug params
+
   puts '----------------------------------------------'
   puts 'status_callback'
   puts params
