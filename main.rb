5dc2a2c6a455c0d2896619a3da5d86dcaf11116c
a.harada <redhornet96@gmail.com>
2016-01-10 16:50:44 +0900

    receive status callback

--- main.rb
+++ main.rb
@@ -11,3 +11,9 @@ end
 get '/call' do
   TwilioCaller.new.call(params[:tel_number]) if params[:tel_number]
 end
+
+post '/status_callback' do
+  puts '----------------------------------------------'
+  puts 'status_callback'
+  puts params
+end
